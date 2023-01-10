a0f35bb138a300442e9caccdce1f5f6963f93edb
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2023-01-10 17:29:40 +0530

    add demo data

---
+++ src/ImgSlider.js
@@ -0,0 +1,33 @@
+// import React from "react";
+// import Slider from "react-slick";
+// import item from "./list";
+
+// const ImgSlider = () => {
+//   const settings = {
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         <h1>React Slicker Card</h1>
+
+//         <Slider {...settings}>
+//           {product.map((item, index) => {
+//             return (
+//               <div>
+//                 <h1>{index}</h1>
+//               </div>
+//             );
+//           })}
+//         </Slider>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default ImgSlider;
